b14964f76c53fd397a5fd47539913e29dd848778
Mamoun Hisham <imnotchoun@gmail.com>
2024-07-24 17:01:27 +0200

    registeration completed

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
 
 Route::get('/admin', function () {
     return view('admin.index');
 });
 
 Route::resource('/admin/products', ProductController::class);
-Route::resource('/admin/categories', CategoryController ::class);
+Route::resource('/admin/categories', CategoryController::class);
