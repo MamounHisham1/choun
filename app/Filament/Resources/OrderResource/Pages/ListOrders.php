b76bb760a7b273ca5a91d4887a2b4e7e0808a52f
Mamoun Hisham <mamounprogrammer@gmail.com>
2024-08-10 00:54:11 +0200

    last commit

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Models\Order;
 use Filament\Actions;
 use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
@@ -16,15 +17,25 @@ class ListOrders extends ListRecords
         return [
             'all' => Tab::make('All orders'),
             'pending' => Tab::make('Pending')
-            ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Pending)),
+                ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Pending))
+                ->badge(fn() => Order::where('status', \App\OrderStatus::Pending)->count())
+                ->badgeColor('warning'),
             'approved' => Tab::make('Approved')
-            ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Approved)),
+                ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Approved))
+                ->badge(fn() => Order::where('status', \App\OrderStatus::Approved)->count())
+                ->badgeColor('success'),
             'shipped' => Tab::make('Shipped')
-            ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Shipped)),
+                ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Shipped))
+                ->badge(fn() => Order::where('status', \App\OrderStatus::Shipped)->count())
+                ->badgeColor('primary'),
             'arrived' => Tab::make('Arrived')
-            ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Arrived)),
+                ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Arrived))
+                ->badge(fn() => Order::where('status', \App\OrderStatus::Arrived)->count())
+                ->badgeColor('gray'),
             'canceled' => Tab::make('Canceled')
-            ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Canceled)),
+                ->modifyQueryUsing(fn($query) => $query->where('status', \App\OrderStatus::Canceled))
+                ->badge(fn() => Order::where('status', \App\OrderStatus::Canceled)->count())
+                ->badgeColor('danger'),
         ];
     }
 
