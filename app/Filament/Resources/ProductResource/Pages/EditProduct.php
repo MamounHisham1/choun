f1abe584b3d9ead7b61384cc67c2b83ed37a35cb
Mamoun Hisham <mamounprogrammer@gmail.com>
2024-08-30 19:30:26 +0200

    fill product attributes in edit fixed

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
+use App\Models\Product;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -16,5 +17,20 @@ class EditProduct extends EditRecord
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function afterFill()
+    {  
+        $product = Product::find($this->data['id']);
+        $test = [];
+        foreach($product->attributes as $attribute) {
+            $test[] = [
+                'product_id' => $attribute->pivot->product_id,
+                'attribute_id' => $attribute->pivot->attribute_id,
+                'values' => json_decode($attribute->pivot->values),
+            ];
+        }
+        $this->data['attributes'] = $test;
+        return $this->data;
     }
 }
