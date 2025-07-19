c676063915051d21cd961ebd38f281cb1770b143
Mamoun Hisham <mamounprogrammer@gmail.com>
2025-07-19 04:34:35 +0300

    test

--- database/migrations/2024_10_07_233109_add_slug_to_brands_table.php
+++ database/migrations/2024_10_07_233109_add_slug_to_brands_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::table('brands', function (Blueprint $table) {
             $table->string('slug')->default('N/A');
+
+            
         });
     }
 
