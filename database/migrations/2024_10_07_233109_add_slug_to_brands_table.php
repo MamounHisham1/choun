3801ad472db33adac55ec8ea7c6d1d40af7b047b
Mamoun Hisham <mamounprogrammer@gmail.com>
2025-07-19 04:33:19 +0300

    fixing the migration second try

--- database/migrations/2024_10_07_233109_add_slug_to_brands_table.php
+++ database/migrations/2024_10_07_233109_add_slug_to_brands_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('brands', function (Blueprint $table) {
-            $table->string('slug')->nullable();
+            $table->string('slug')->default('N/A');
         });
     }
 
